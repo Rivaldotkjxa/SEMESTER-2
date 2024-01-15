2ca86e5db550c28789500bf4a05aabc788a3d32f
Rivaldotkjxa <143969773+Rivaldotkjxa@users.noreply.github.com>
2024-01-15 09:42:02 +0700

    Add files via upload

---
+++ RIVALDO TKJ/DJJ SEMESTER 2/p1/perkenalan.py
@@ -0,0 +1,6 @@
+nama ="Rivaldo"
+kelas =" TKJ X A"
+jurusan ="Teknik komputer dan jaringan"
+
+print("perkenalkan nama saya", nama)
+print("saya kelas",kelas,"Jurusan", jurusan)
---
+++ RIVALDO TKJ/DJJ SEMESTER 2/p1/variable.py
@@ -0,0 +1,19 @@
+x = 7
+y = 2
+z = "SMK MEDIKAKOM"
+P = False
+q = True
+
+print(x)
+print(y)
+print(z)
+print(P)
+print(q)
+
+print("cek tipe data")
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(P))
+print(type(q))
+
