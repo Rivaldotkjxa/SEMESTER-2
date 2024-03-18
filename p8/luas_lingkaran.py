87ae19b53194ccde21eb7c60713a96811622dfb2
Rivaldotkjxa <143969773+Rivaldotkjxa@users.noreply.github.com>
2024-03-18 09:01:40 +0700

    Add files via upload

---
+++ p8/luas_lingkaran.py
@@ -0,0 +1,4 @@
+def luas_lingkaran(radius):
+    return 3.14 * radius * radius
+
+print("luas lingklaran:", luas_lingkaran(3))
---
+++ p8/perkenalan.py
@@ -0,0 +1,6 @@
+def perkenalan(nama, kelas, umur):
+    print("nama saya", nama)
+    print("saya kelas", kelas)
+    print("saya berusia", umur)
+    
+perkenalan("rivaldo", "TKJ X A", "17 tahun")    
