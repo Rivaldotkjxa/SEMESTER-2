dc08f1e22d9501c24ff40e62ae471b5ab5cd16e3
Rivaldotkjxa <143969773+Rivaldotkjxa@users.noreply.github.com>
2024-04-22 09:30:07 +0700

    Add files via upload

---
+++ function/function_2.py
@@ -0,0 +1,6 @@
+def perkenalan(nama):
+    print("perkenalan nama saya", nama)
+    
+perkenalan("nida")
+perkenalan("rosa")    
+    
---
+++ function/function_3.py
@@ -0,0 +1,9 @@
+def berangkat_sekolah(pakaian,buku):
+    if pakaian == "seragam" and buku == "bawa":
+        print("berangkat sekolah ")
+    else:
+        print("pergi main")
+     
+berangkat_sekolah("seragham","bawa")
+berangkat_sekolah("jeans","tidak membawa buku")        
+            
